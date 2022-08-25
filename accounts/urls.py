5c5093079fb326e445d9daae6a20d346148f93d6
NYikolay <kolyangrans@mail.ru>
2022-08-25 16:53:58 +0300

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('registrer/', CreateUser.as_view(), name='registration')
+    path('register/', CreateUser.as_view(), name='registration')
 ]
