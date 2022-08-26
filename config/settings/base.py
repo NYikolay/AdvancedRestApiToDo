da67b2ce3b4214f493c47d74bfbdb077e007d005
NYikolay <kolyangrans@mail.ru>
2022-08-26 22:29:02 +0300

    added filters and search

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     'django_extensions',
+    'django_filters',
 
     'accounts',
     'main',
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import viewsets, filters
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 
 from main.models import Category, Task
@@ -14,7 +16,7 @@ import json
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = [IsOwner]
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [SearchFilter]
     search_fields = ['name']
 
     def get_queryset(self):
@@ -22,15 +24,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    """
-    Depending on the category query, ?category=id returns linked jobs
-    """
     serializer_class = TaskSerializer
     permission_classes = [IsOwner]
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_fields = ['is_done', 'priority']
     search_fields = ['name']
 
     def get_queryset(self):
+        """
+        If the ?category=id parameter came in, it returns jobs depending
+        on the current category otherwise it returns all jobs of the current user
+        """
         category = self.request.query_params.get('category')
         if category:
             return Task.objects.filter(owner=self.request.user, category__id=category)
@@ -39,6 +43,9 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def get_incomplete(self, request):
+        """
+        Return incomplete tasks for current user
+        """
         count = Task.objects.filter(owner=self.request.user, is_done=False).count()
 
         data = {
