a589260bdd77d2a396ebf9c22d855692775dabb2
NYikolay <kolyangrans@mail.ru>
2022-09-01 10:41:32 +0300

    added category statistic

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 
-from main.views import CategoryViewSet, TaskViewSet, PriorityViewSet
+from main.views import CategoryViewSet, TaskViewSet, PriorityViewSet, TaskStatistic
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
@@ -10,6 +10,7 @@ router.register(r'tasks', TaskViewSet, basename='tasks')
 router.register(r'priorities', PriorityViewSet, basename='priorities')
 
 urlpatterns = [
+    path('category_statistic/', TaskStatistic.as_view())
 ]
 
 urlpatterns += router.urls
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from main.models import Category, Task
 from main.permissions import IsOwner
@@ -61,6 +62,29 @@ class PriorityViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.owner_priority.all()
+
+
+class TaskStatistic(APIView):
+
+    def get(self, request, format=None):
+        current_category = request.query_params.get("category", None)
+        try:
+            tasks = Task.objects.filter(category_id=current_category).count()
+            tasks_done = Task.objctes.filter(category_id=current_category, is_done=True).count()
+            incompleted_tasks = Task.objects.filter(category_id=current_category, is_done=False).count()
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        data = {
+            'tasks_count': tasks,
+            'tasks_done': tasks_done,
+            'incompleted_tasks': incompleted_tasks
+        }
+
+        return Response(data)
+
+
+
 
 
 
